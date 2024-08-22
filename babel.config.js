7747f03a2c3e819037a2f259705916967a799094
Sunkanmi <sunkanmi.oguntimehin@optisoft.ng>
2024-08-22 21:01:10 +0100

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
+const { plugins } = require("./tailwind.config");
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins:["nativewind/babel"]
   };
 };
